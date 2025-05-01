9e519929dfc0edd2d97198c2c4d847eaef5fcaec
Luana Ortubia <lulyortubia2002@gmail.com>
2025-05-01 17:13:43 -0300

    Cambio de mensaje desde experimento_rebase

--- main.dart
+++ main.dart
@@ -1,5 +1,9 @@
 //import 'package:flutter/material.dart';
 
 void main() {
+<<<<<<< HEAD
   print('CAMBIO DE MENSAJE');
+=======
+  print('CAMBIO DE MENSAJE EN LA RAMA EXPERIMENTO REBASE');
+>>>>>>> d870437 (Cambio de mensaje desde experimento_rebase)
 }
